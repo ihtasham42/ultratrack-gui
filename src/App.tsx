2d85adc6b1f7b2f00292a7c7d1376a5603d10b7f
Ihtasham <chowdhuryihtasham42@gmail.com>
2023-11-15 22:39:42 +0000

    Setup css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import React from "react";
+import { Button, SegmentedControl } from "@mantine/core";
 
-
-function App() {
+const App = () => {
   return (
-    <>
-  
-    </>
-  )
-}
+    <div>
+      <div> hello</div>
+      <Button>Test</Button> 
+      <SegmentedControl data={['React', 'Angular', 'Vue']} />
+    </div>
+  );
+};
 
-export default App
+export default App;
